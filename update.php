d7e26c946328144ac7c1954392cce803bf73f75b
alphastockid0 <62582615+alphastockid0@users.noreply.github.com>
2020-03-27 05:36:09 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,17 +1,13 @@
 <?php
 cd
-$remote = " git remote add github https://github.com/alphastockid0/GJK.git "
-  $remote
-  if ($remote==false){goto nextt}
-else{
-  nextt:
-git remote -v
-git pull github master}
-
-
-
-
-
-
+$remote = "git remote add github https://github.com/alphastockid0/GJK.git";
+  $remote;
+  if ($remote==false){
+    goto nextt;
+  } else{
+    nextt:
+    git remote -v;
+    git pull github master;
+    }
 
 ?>
