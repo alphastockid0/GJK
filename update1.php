8e00b8c0a931efbf39f77564ffe332796872e5c0
alphastockid0 <62582615+alphastockid0@users.noreply.github.com>
2020-03-27 06:41:20 +0700

    Update update1.php

--- update1.php
+++ update1.php
@@ -126,11 +126,11 @@ function change(){
                 //                          else if($pilih1 == "n" || $pilih1 == "N"){
                 //                                  die();}
                 //                               else{ echo color("red","-] GAGAL!!!\n");}
-                }
+               // }
          }
          }
          }
-                else{goto setpin;}
+                else {goto setpin;}
                 
          }
             else{
